623a856d0e5ab5e736687c841d99e9fb2a2a1802
Bruno Santos <bruno_reworks@outlook.com>
2023-02-27 22:08:52 -0300

    alterações e js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function total() {
+    let total = 500;
+    let check = document.getElementsByName("checkbox");
+    
+    for (var i = 0; i < check.length; i++) {
+        if (check[i].checked) {
+        total += parseInt(check[i].value);
+        }
+    }
+    console.log("R$ " + total);
+}
