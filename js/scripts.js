b3e1347ba3d4d8eebd0165190bd23d9f8686d225
Bruno Santos <bruno_reworks@outlook.com>
2023-02-28 22:50:34 -0300

    construtor com checkbox funcionando

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
-function total() {
-    let total = 500;
-    let check = document.getElementsByName("checkbox");
-    
-    for (var i = 0; i < check.length; i++) {
-        if (check[i].checked) {
-        total += parseInt(check[i].value);
+//função para calcular os checkbox / total//
+class Calculator {
+    constructor(total) {
+        this.check = document.getElementsByName("checkbox");
+        this.total = total;
+    }
+// Calculando o total de acordo com o checkbox selecionado na página "Orcamento.html"
+    calcularTotal() {
+        this.total = 500;
+        for (var i = 0; i < this.check.length; i++) {
+            if (this.check[i].checked) {
+                this.total += parseInt(this.check[i].value);//Somando o valor de cada checkbox ao total
+            }
         }
+        //Convertendo o valor final em moeda
+        const opcoes = { style: 'currency', currency: 'BRL' };
+        const valorFormatado = this.total.toLocaleString('pt-BR', opcoes);
+        console.log(valorFormatado);
+
     }
-    console.log("R$ " + total);
-    }
+}
+
+const valorAtualizado = new Calculator();
