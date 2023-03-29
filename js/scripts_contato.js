03f88af07b3526c7580b2b675cd30ac36f9b6592
Bruno Santos <bruno_reworks@outlook.com>
2023-03-29 16:59:28 -0300

    alerta pagina contato e pequenas alterações no JS de login

--- js/scripts_contato.js
+++ js/scripts_contato.js
@@ -1,23 +1,27 @@
 //Capturar os dados do formulário e armazenar no localStorage, em seguida enviar alert confirmando o envio do formulário.
 
 function enviarFormulario() {
-    const formulario = {
-      nome: document.getElementById('nome_form').value,
-      email: document.getElementById('email_form').value,
-      solicitacao: document.getElementById('solicitacao_form').value,
-      comentario: document.getElementById('comentario_form').value
-    };
+  const formulario = {
+    nome: document.getElementById('nome_form').value,
+    email: document.getElementById('email_form').value,
+    solicitacao: document.getElementById('solicitacao_form').value,
+    comentario: document.getElementById('comentario_form').value
+  };
 
-    localStorage.setItem('formulario', JSON.stringify(formulario));
+  localStorage.setItem('formulario', JSON.stringify(formulario));
 
-    alert('Formulário enviado com sucesso!');
-  }
+  Swal.fire({
+    title: 'Formulário enviado com sucesso!',
+    icon: 'success',
+    confirmButtonText: 'OK'
+  });
+}
 
-  const formularioSalvo = JSON.parse(localStorage.getItem('formulario'));
+const formularioSalvo = JSON.parse(localStorage.getItem('formulario'));
 
-  if (formularioSalvo) {
-    document.getElementById('nome_form').value = formularioSalvo.nome;
-    document.getElementById('email_form').value = formularioSalvo.email;
-    document.getElementById('solicitacao_form').value = formularioSalvo.solicitacao;
-    document.getElementById('comentario_form').value = formularioSalvo.comentario;
-  }
+if (formularioSalvo) {
+  document.getElementById('nome_form').value = formularioSalvo.nome;
+  document.getElementById('email_form').value = formularioSalvo.email;
+  document.getElementById('solicitacao_form').value = formularioSalvo.solicitacao;
+  document.getElementById('comentario_form').value = formularioSalvo.comentario;
+}
--- js/scripts_login.js
+++ js/scripts_login.js
@@ -1,51 +1,69 @@
-//desafio 11
-
-class formLogin {
-    constructor(emailID, passwordID, submitID) {
-        this.email = document.querySelector(`#${emailID}`);
-        this.password = document.querySelector(`#${passwordID}`);
-        this.submit = document.querySelector(`#${submitID}`);
-        //adicionando evento de click ao botão de login, que chama a funcao pressSubmit (botao)
-        this.submit.addEventListener('click', this.pressSubmit.bind(this));
-
-        //chamando funcoes de validacao e funcao de converter os valores para JSON
-        this.validation()
-        this.convertJson()
+class FormLogin {
+    constructor(emailId, passwordId, submitId) {
+      this.emailInput = document.querySelector(`#${emailId}`);
+      this.passwordInput = document.querySelector(`#${passwordId}`);
+      this.submitButton = document.querySelector(`#${submitId}`);
+  
+      // Adiciona evento de clique ao botão de login
+      this.submitButton.addEventListener('click', this.handleLogin.bind(this));
     }
-
-    pressSubmit(event) {
-        //preventDefault sendo usado para impedir o carregamento da página ao apertar o botao de login
-        event.preventDefault();
-
-        //Atribuindo valores para email e senha e armazenando no localStorage
-        localStorage.setItem('email', this.email.value);
-        localStorage.setItem('senha', this.password.value);
-
-        //chamando as funcoes novamente para que seja feita a atualizacao apos o click no botao de login 
-        this.validation()
-        this.convertJson()
+  
+    handleLogin(event) {
+      event.preventDefault();
+  
+      const email = this.emailInput.value;
+      const password = this.passwordInput.value;
+  
+      if (!email || !password) {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Por favor, preencha os campos de e-mail e senha.',
+          })
+        return;
+      }
+  
+      try {
+        localStorage.setItem('email', email);
+        localStorage.setItem('senha', password);
+        this.convertToJson();
+  
+        // Alerta de login efetuado com sucesso
+        Swal.fire({
+            icon: 'success',
+            title: 'Login efetuado com sucesso!',
+          })
+      } catch (error) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Não foi possível salvar o usuário e a senha.',
+          })
+      }
     }
-
-    validation() {
-        //validando se o localStorage está vazio
-        if (localStorage.getItem('email') && localStorage.getItem('senha') != null) {
-            this.email.value = `${localStorage.getItem('email')}`;
-            this.password.value = `${localStorage.getItem('senha')}`;
-        }
+  
+    convertToJson() {
+      const email = localStorage.getItem('email');
+      const senha = localStorage.getItem('senha');
+  
+      const userData = { email, senha };
+      const jsonData = JSON.stringify(userData);
+  
+      console.log(jsonData);
     }
-    //funcao para transformar os dados do localStorage em um JSON
-    convertJson(dadosAcesso) {
-        let arrayDados = [
-            { email: `${localStorage.getItem('email')}` },
-            { senha: `${localStorage.getItem('senha')}` }
-        ]
-        dadosAcesso = JSON.stringify(arrayDados);
-        console.log(dadosAcesso);
-    }
-}
-//chamando a classe e definindo o valor dos parametros (ids)
-const dataLogin = new formLogin('inputEmail', 'inputPassword', 'submitButton');
-
-
-
-
+  }
+  
+  const form = new FormLogin('inputEmail', 'inputPassword', 'submitButton');
+  
+  const resetPasswordButton = document.getElementById('resetPassword');
+  
+  resetPasswordButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    Swal.fire({
+      title: 'Esqueceu sua senha?',
+      text: 'Digite o e-mail abaixo para receber um link de recuperação',
+      input: 'email',
+      showCancelButton: true,
+      confirmButtonText: 'Enviar',
+      cancelButtonText: 'Cancelar',
+    });
+  });
+  
